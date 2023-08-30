a60d25ebffa891dc6c61b56be9fa9bec06d0af30
arizona <aaronsmatharoo@gmail.com>
2023-08-30 13:32:31 -0400

    added touch for mobile

--- script.js
+++ script.js
@@ -6,6 +6,30 @@ const playerPaddle = new Paddle(document.getElementById("player-paddle"))
 const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
 const playerScoreElem = document.getElementById("player-score")
 const computerScoreElem = document.getElementById("computer-score")
+document.addEventListener("touchstart", handleTouchStart)
+document.addEventListener("touchmove", handleTouchMove)
+
+let touchStartY = null
+
+function handleTouchStart(event) {
+    touchStartY = event.touches[0].clientY
+}
+
+function handleTouchMove(event) {
+    if (!touchStartY) {
+        return
+    }
+
+    const touchY = event.touches[0].clientY
+    const deltaY = touchY - touchStartY
+
+    // Adjust the paddle position based on deltaY
+    const newPaddlePosition = playerPaddle.position + (deltaY / window.innerHeight) * 100
+    playerPaddle.position = Math.min(Math.max(newPaddlePosition, 0), 100)
+
+    touchStartY = touchY
+}
+
 
 let lastTime
 function update(time) {
@@ -43,8 +67,11 @@ function handleLose() {
     computerPaddle.reset()
 }
 
-document.addEventListener("mousemove", e => {
-    playerPaddle.position = (e.y / window.innerHeight) * 100
-})
+document.addEventListener("mousemove", handleMouseMove)
+
+function handleMouseMove(event) {
+    const newY = (event.clientY / window.innerHeight) * 100
+    playerPaddle.position = Math.min(Math.max(newY, 0), 100)
+}
 
 window.requestAnimationFrame(update)
